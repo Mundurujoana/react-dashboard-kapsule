d23b5ba234ba171a5707b3cf7c7bdaa041ce7932
Mundurujoana <mundujoana@gmail.com>
2022-12-01 11:36:24 +0300

    commit changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,9 +2,9 @@ import react, { useState, useEffect } from 'react'
 import './Home.css'
 import Widget from '../../components/Widget/Widget'
 import BarChart from '../Charts/BarChart'
-// import { AppData } from '../Charts/Data'
 import PieChart from '../Charts/PieChart'
-import { auth, db } from '../../Firebase'
+// import { AppData } from '../Charts/Data'
+import { db } from '../../Firebase'
 import { query, collection, getDocs, doc, where, getDoc, } from "firebase/firestore";
 import { useUserAuth } from "../../context/UserAuthContext";
 
@@ -14,115 +14,82 @@ const Home = () => {
   const { user } = useUserAuth();
   const [userInfo, setUserInfo] = useState([])
   const [graphs, setGraphs] = useState([])
-  const [all, setAll] = useState();
   const [currentUserLocation, setCurrentUserLocation] = useState('');
   const [AppData,setAppData] =useState([]);
+  const [name,setName] =useState("");
   
   
+  const graphStyle = {
+    textAlign : "center",
+    fontWeight: 600
+  };
+
   //get the specific loggedIn user id and the location
   const loggedInUser = userInfo?.find(info => info.uid === user?.uid)
-  // console.log(loggedInUser?.uid)
+  console.log(loggedInUser)
   //get the the location of a specific loggedIn user
-  const graphData = graphs?.find(graph => graph.id === loggedInUser?.location)
+  // const graphData = graphs?.find(graph => graph.id === loggedInUser?.location)
   // console.log(graphData)
 
-
-  // const fetchUsers = async() => {
-    // const userRef = await getDocs(collection(db, "users")).then((querySnapshot)=>{             
-    //             const newData = querySnapshot.docs
-    //                 .map((doc) => ({...doc.data()}))
-    //             setUserInfo(newData)          
-    //         })
-    // const q = query(collection(db, "users"), where("uid", "==", user?.uid));
-    //   const docSnap = await getDocs(q);
-    //   const userData = docSnap.docs[0].data() //
-    //   console.log(userDat)
-  // }
 
   
   //fetch data from user
   const fetchUsers = async () => {
     const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+    
       const docSnap = await getDocs(q);
       const userData = docSnap.docs[0].data()
       console.log(userData.location,"From fetch user")
       setCurrentUserLocation(userData.location);
+      setName(userData.location)
   }
+  console.log(name, 'name of the loggedin user location')
 
-  console.log(currentUserLocation,"Current user Location")
-
-
-
-
-  //fetch data from graph
-  // const fetchGraphs = async () => {
-  // const [currentUserLocation, setCurrentUserLocation] = useState('');
-  //   const graphRef = await getDocs(collection(db, "graphs/"+currentUserLocation)).then((querySnapshot) => {
-  //     const newGraphData = querySnapshot.docs
-  //       .map((doc) => {
-  //         if(doc.data().location === currentUserLocation){
-  //          return { ...doc.data(), location: doc.id, status: "ok" }
-  //         }
-  //       })
-  //     setGraphs(newGraphData)
-  //     // newGraphData.map((item) => {
-  //     //   console.log(item.location)
-  //     //   console.log(user.location, 'current user')
-  //     // })
-  //     // console.log(newGraphData)
-  //   })
-  // }
 
   const handleSyncData = async () => {
     if(currentUserLocation !== ''){
-      const dataRef = doc(db, `graphs/Kenya`); 
+      const dataRef = await doc(db, `graphs/${currentUserLocation}`); 
       const docSnap = await getDoc(dataRef);
       if (docSnap.exists()) {
         const result = docSnap.data();
+        console.log(result)
         setAppData(result.data)
       }
     }
-  
   }
-  
+    console.log(currentUserLocation,"is the current user location")
+
 
   fetchUsers();
   //rendering data from firestore
   useEffect(() => {
     handleSyncData();
-    // fetchGraphs()
-    // getAll()
-  }, [])
-
-  //mapping through the userInfo to obtain location and eliminate duplication
-  const labs = userInfo.map((dat) => dat.location)
-  let uniqueLabs = [...new Set(labs)];
-
+  }, [currentUserLocation])
 
 
 
   //mapping through the userInfo to obtain location and eliminate duplicatiob
-  const ulabs = AppData.map((dat) => dat.year)
-  console.log(ulabs)
+  const userYears = AppData.map((dat) => dat.year)
+  console.log(userYears)
 
-  const dlabs = AppData.map((dat) => dat.userGain)
-  console.log(dlabs)
+  const userGain = AppData.map((dat) => dat.userGain)
+  console.log(userGain)
 
-  const slabs = AppData.map((dat) => dat.userLost)
-  console.log(slabs)
+  const userLost = AppData.map((dat) => dat.userLost)
+  console.log(userLost)
 
   //Data for barGraphs
   const barState = {
-    labels: ulabs,
+    labels: userYears,
     datasets: [
       {
-        label: 'Users Gained',
-        data: dlabs,
+        label: `Users Gained in ${name}`,
+        data: userGain,
         backgroundColor: [
           "#ffffff",
-          "#000000",
-          "#50AF95",
-          "#c5ab3a",
+          "#6a2c2c",
+          "#2c3230",
+          "#5a5127",
 
         ],
         borderColor: 'rgba(0,0,0,1)',
@@ -133,13 +100,13 @@ const Home = () => {
 
   //Data for pieChart
   const pieState = {
-    labels: ulabs,
+    labels: userYears,
     datasets: [
       {
-        label: 'Users Lost',
-        data: slabs,
+        label: `Users Lost in ${name}`,
+        data: userLost,
         backgroundColor: [
-          "#ffffff",
+          "#485c71",
           "#000000",
           "#50AF95",
           "#c5ab3a",
@@ -156,15 +123,14 @@ const Home = () => {
       <Widget />
 
       <div className='chart-container'>
-        {graphData ? (
-          <div style={{ width: 550, marginTop: 50, marginLeft: 200 }}>
+          <div style={{ width: 500, marginTop: 90, marginLeft: 70 }}>
             <BarChart chartData={barState} />
+            <p  style={graphStyle} > Users Gained in {name} </p>
           </div>
-        ) : (
-          <div style={{ width: 550, marginTop: 50, marginLeft: 200 }}>
-            <BarChart chartData={pieState} />
+          <div style={{ width: 300, marginTop: 50, marginLeft: 100 }}>
+            <PieChart chartData={pieState} />
+          <p style={graphStyle}> Users Lost in {name} </p>
           </div>
-        )}
       </div>
     </div>
   )
